6e87268930304bb55de5b5830a5209d0b83ba226
Arzumify <jliwin98@hectabit.org>
2024-04-29 21:18:46 +0100

    Made aeskeyshare work properly

--- static/js/aeskeyshare.js
+++ static/js/aeskeyshare.js
@@ -1,24 +1,17 @@
+// Add an event listener to handle incoming messages
 window.addEventListener("message", function(event) {
-    try {
-        let data = JSON.parse(event.data);
-        const access_token = data["access_token"];
-        const redirect_uri = data["redirect_uri"];
+    const access_token = event.data;
 
-        fetch("https://auth.hectabit.org/api/isloggedin", {
-            method: "POST",
-            body: JSON.stringify({
-                access_token: access_token
-            })
+    fetch("https://auth.hectabit.org/api/isloggedin", {
+        method: "POST",
+        body: JSON.stringify({
+            access_token: access_token
         })
-            .then((response) => {
-                if (response.status === 200) {
-                    console.log("Key is valid");
-                    let newtab = window.open(redirect_uri);
-                    newtab.postMessage(localStorage.getItem("DONOTSHARE-password"), "*");
-                    window.close();
-                }
-            });
-    } catch {
-        console.log("Error parsing JSON");
-    }
+    })
+    .then((response) => function () {
+        if (response.status === 200) {
+            console.log("Key is valid")
+            window.postMessage(localStorage.getItem("DONOTSHARE-password"), event.origin)
+        }
+    })
 });
